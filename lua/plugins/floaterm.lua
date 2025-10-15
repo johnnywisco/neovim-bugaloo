96d37cc50f86c0bda8b1929fed125b0bd58d5e77
John Sanfelippo <john.sanfelippo@molsoncoors.com>
2025-10-15 08:18:36 -0500

    Updated floaterm settings

--- lua/plugins/floaterm.lua
+++ lua/plugins/floaterm.lua
@@ -2,9 +2,16 @@ return {
     {
         "nvzone/floaterm",
         dependencies = "nvzone/volt",
-        opts = {
-            size = { h = 80, w = 80 },
-        },
-        cmd = "FloatermToggle",
-    }
+        config = function()
+            require('floaterm').setup {
+                boarder = 'double',
+                size = { h = 60, w = 70 },
+            }
+        end
+
+        -- opts = {
+        --     boarder = true,
+        --     size = { h = 80, w = 80 },
+        -- },
+    },
 }
